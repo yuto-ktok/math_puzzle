c66ddaada371282fb759778a683a4fd5364c2ce5
片岡　悠人 <yuto_kataoka@r.recruit.co.jp>
2016-04-13 12:32:12 +0900

    式間違えてた修正

--- code_iq/one_line_circle2.py
+++ code_iq/one_line_circle2.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 import math
 l = int(raw_input())
-print l * sum(map(lambda x: x*(l-3), range(1,l-2))) * math.factorial(l-3)
+print l * sum(map(lambda x: x*(l-2-x), range(1,l-2))) * math.factorial(l-3)
